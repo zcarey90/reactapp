d5296cd171a6af5af1d78a207d7e50b015ec79bc
Zacharey Carey <zac4ncstate@aol.com>
2020-04-02 14:09:56 -0400

    improved Header

--- my-react-app/src/components/layout/Header.js
+++ my-react-app/src/components/layout/Header.js
@@ -2,10 +2,17 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header style={chartStyle}>
       <h1>ChoreChart</h1>
     </header>
   );
 }
+
+const chartStyle = {
+  background: "black",
+  color: "purple",
+  textAlign: "center",
+  padding: "10px"
+};
 
 export default Header;
