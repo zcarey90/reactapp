778df107049f272564893991dda15baefcf6444d
Zacharey Carey <zac4ncstate@aol.com>
2020-04-02 13:56:01 -0400

    added a Header

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/layout/Header";
 import Chores from "./components/Chores";
 
 import "./App.css";
@@ -43,6 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Chores
           chores={this.state.chores}
           checkDone={this.checkDone}
