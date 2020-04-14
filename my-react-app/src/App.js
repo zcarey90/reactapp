2ecf494c2e65303cd3398d506b7a72c1ee3e766b
Zacharey Carey <zac4ncstate@aol.com>
2020-04-14 16:51:58 -0400

    almost done

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,70 +1,83 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Chores from "./components/Chores";
 import AddChore from "./components/AddChore";
-import uuid from "uuid";
+import About from "./components/chapters/About";
+import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 class App extends Component {
   state = {
     chores: [
       {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: "Clean garage",
-        completed: false
+        completed: false,
       },
       {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: "Take the dog for a walk",
-        completed: false
+        completed: false,
       },
       {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: "Wash my car",
-        completed: false
-      }
-    ]
+        completed: false,
+      },
+    ],
   };
 
-  checkDone = id => {
+  checkDone = (id) => {
     this.setState({
-      chores: this.state.chores.map(chore => {
+      chores: this.state.chores.map((chore) => {
         if (chore.id === id) {
           chore.completed = !chore.completed;
         }
         return chore;
-      })
+      }),
     });
   };
 
-  delChore = id => {
+  delChore = (id) => {
     this.setState({
-      chores: [...this.state.chores.filter(chore => chore.id !== id)]
+      chores: [...this.state.chores.filter((chore) => chore.id !== id)],
     });
   };
 
-  addChore = title => {
+  addChore = (title) => {
     const newChore = {
-      id: uuid.v4(),
+      id: uuidv4(),
       title,
-      completed: false
+      completed: false,
     };
     this.setState({ chores: [...this.state.chores, newChore] });
   };
 
   render() {
     return (
-      <div className="App">
-        <div className="receptacle">
-          <Header />
-          <AddChore addChore={this.addChore} />
-          <Chores
-            chores={this.state.chores}
-            checkDone={this.checkDone}
-            delChore={this.delChore}
-          />
+      <Router>
+        <div className="App">
+          <div className="receptacle">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddChore addChore={this.addChore} />
+                  <Chores
+                    chores={this.state.chores}
+                    checkDone={this.checkDone}
+                    delChore={this.delChore}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- my-react-app/src/components/layout/Header.js
+++ my-react-app/src/components/layout/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header style={chartStyle}>
       <h1>Chore Chart</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>
+      |
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
     </header>
   );
 }
@@ -12,7 +20,12 @@ const chartStyle = {
   background: "black",
   color: "purple",
   textAlign: "center",
-  padding: "10px"
+  padding: "10px",
+};
+
+const linkStyle = {
+  color: "blue",
+  textDecoration: "none",
 };
 
 export default Header;
